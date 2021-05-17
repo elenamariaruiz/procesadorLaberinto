30eb00d11c59e4fa48a6a6c04ce2d997c336b76c
Elena Ruiz <elenamaria.ruiz@alu.uclm.es>
2021-05-17 14:14:10 +0200

    cambios en ANTLR

---
+++ antlr/MyLaberintoErrorListener.py
@@ -0,0 +1,6 @@
+from LaberintoListener import LaberintoListener
+from LaberintoParser import LaberintoParser
+
+class LaberintoErrorListener(LaberintoListener):
+    def __init__(self) -> None:
+        super().__init__()
