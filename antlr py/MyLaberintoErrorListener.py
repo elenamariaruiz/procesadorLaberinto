80efca7a21bcb47a918fc68f223b312355fdb90b
Elena Ruiz <elenamaria.ruiz@alu.uclm.es>
2021-06-28 18:14:37 +0200

    Organización del proyecto

--- antlr py/MyLaberintoErrorListener.py
+++
@@ -1,6 +0,0 @@
-from LaberintoListener import LaberintoListener
-from LaberintoParser import LaberintoParser
-
-class LaberintoErrorListener(LaberintoListener):
-    def __init__(self) -> None:
-        super().__init__()
